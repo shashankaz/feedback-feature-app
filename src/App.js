e631633b4eed55c55bb0711d843451adc3fc46e3
Shashank <113260615+shashankaz@users.noreply.github.com>
2024-06-11 21:09:37 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
     <div>
       <div className={`main-body ${open ? "overlay" : ""}`}>
         <Navbar />
+        <section>Feedback Feature App</section>
       </div>
       <Fab
         issueValidate={issue}
